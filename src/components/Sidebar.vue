a4d3e6b2fc369a5a8a8d31582ab7ac1e42b64b9a
GustaBarbosaa <127910329+GustaBarbosaa@users.noreply.github.com>
2024-06-09 15:35:08 -0300

    Update Sidebar.vue

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -7,7 +7,7 @@
 
 		<div class="menu-toggle-wrap">
             <div class="projeto">
-                <h1>AGILIZA-AE</h1>
+                <h1>Agiliza-Aê</h1>
             </div>
 			<button class="menu-toggle" @click="ToggleMenu">
 				<span class="material-icons">keyboard_double_arrow_right</span>
@@ -16,7 +16,7 @@
 
 		<h3>Menu</h3>
 		<div class="menu">
-			<router-link to="/" class="button">
+			<router-link to="/home" class="button">
 				<span class="material-icons">home</span>
 				<span class="text">Home</span>
 			</router-link>
@@ -41,7 +41,7 @@
 
 <script setup>
 import { ref } from 'vue'
-import logoURL from '../assets/logo.png'
+import logoURL from '../assets/fulltime_agiliza.svg'
 
 const is_expanded = ref(localStorage.getItem("is_expanded") === "true")
 
@@ -51,162 +51,185 @@ const ToggleMenu = () => {
 }
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 aside {
-	display: flex;
-	flex-direction: column;
+  display: flex;
+  flex-direction: column;
+  background-color: transparent;
+  color: transparent; 
+  width: calc(2rem + 32px);
+  overflow: hidden;
+  min-height: 100vh;
+  padding: 1rem;
+  transition: 0.5s ease-in-out;
+  position: relative;
 
-	background-color: var(--dark);
-	color: var(--light);
+  &:after {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: transparent;
+    transition: background-color 0.5s ease-in-out;
+    z-index: -1;
+  }
 
-	width: calc(2rem + 32px);
-	overflow: hidden;
-	min-height: 100vh;
-	padding: 1rem;
+  &:hover:after {
+    background-color: #bdbbbb; /* Background semi-transparente ao passar o mouse */
+  }
 
-	transition: 0.5s ease-in-out;
+  &.is-expanded {
+    background-color: #bdbbbb; /* Background color ao expandir */
+    width: var(--sidebar-width);
+  }
 
-	.flex {
-		flex: 1 1 0%;
-	}
+  .flex {
+    flex: 1 1 0%;
+  }
 
-	.logo {
-		margin-bottom: 1rem;
-        
+  .logo {
+    margin-bottom: 1rem;
 
-		img {
-			width: 2rem;            
-		}
-	}
-
-    
-	.menu-toggle-wrap {
-		display: flex;
-		justify-content: flex-end;
-		margin-bottom: 1rem;
-		position: relative;
-		top: 0;
-		transition: 0.2s ease-in-out;
-
-		.projeto h1 {
-			font-size: 20px;
-			padding-top: 10px;
-			padding-right: 2.5vw;
-		}
-
-		.menu-toggle {
-			transition: 0.2s ease-in-out;
-			.material-icons {
-				font-size: 2rem;
-				color: var(--light);
-				transition: 0.2s ease-out;
-			}
-			
-			&:hover {
-				.material-icons {
-					color: var(--primary);
-					transform: translateX(0.5rem);
-				}
-			}
-		}
-	}
-
-	
-
-	h3, .button .text {
-		opacity: 0;
-		transition: opacity 0.3s ease-in-out;
-	}
-
-	h3 {
-		color: var(--grey);
-		font-size: 0.875rem;
-		margin-bottom: 0.5rem;
-		text-transform: uppercase;
-        text-align: center;
-        padding-bottom: 5vh;
-        
-	}
-
-	.menu {
-		margin: 0 -1rem;
-
-		.button {
-			display: flex;
-			align-items: center;
-			text-decoration: none;
-
-			transition: 0.2s ease-in-out;
-			padding: 0.5rem 1rem;
-
-			.material-icons {
-				font-size: 2rem;
-				color: var(--light);
-				transition: 0.2s ease-in-out;
-			}
-			.text {
-				color: var(--light);
-				transition: 0.2s ease-in-out;
-			}
-
-			&:hover {
-				background-color: var(--dark-alt);
-
-				.material-icons, .text {
-					color: var(--primary);
-				}
-			}
-
-			&.router-link-exact-active {
-				background-color: var(--dark-alt);
-				border-right: 5px solid var(--primary);
-
-				.material-icons, .text {
-					color: var(--primary);
-				}
-			}
-		}
-	}
-
-	.footer {
-		opacity: 0;
-		transition: opacity 0.3s ease-in-out;
-
-		p {
-			font-size: 0.875rem;
-			color: var(--grey);
-		}
-	}
-
-	&.is-expanded {
-		width: var(--sidebar-width);
-
-		.menu-toggle-wrap {
-			top: -3rem;
-			
-			.menu-toggle {
-				transform: rotate(-180deg);
-			}
-		}
-
-		h3, .button .text {
-			opacity: 1;
-		}
-
-		.button {
-			.material-icons {
-				margin-right: 1rem;
-			}
-		}
-
-		.footer {
-			opacity: 0;
-		}
-	}
-
-	@media (max-width: 1024px) {
+    img {
+		background-color: transparent;
 		position: absolute;
-		z-index: 99;
-	}
+		top: 0vw;
+		left: -1.5vw;
+      width: 6rem;
+    }
+  }
+
+  .menu-toggle-wrap {
+    display: flex;
+    justify-content: flex-end;
+    margin-bottom: 1rem;
+    position: relative;
+    top: 0;
+    transition: 0.2s ease-in-out;
+
+    .projeto h1 {
+      font-size: 20px;
+      padding-top: 31px;
+      padding-right: 3vw;
+	  color: #FFF;
+    }
+
+    .menu-toggle {
+		margin-top: 17px;
+      transition: 0.2s ease-in-out;
+
+      .material-icons {
+        font-size: 2rem;
+        color: #272727; 
+        transition: 0.2s ease-out;
+      }
+
+      &:hover .material-icons {
+        color: #8f0000; /* Substituído por uma cor específica */
+        transform: translateX(0.5rem);
+      }
+    }
+  }
+
+  h3,
+  .button .text {
+    opacity: 0;
+    transition: opacity 0.3s ease-in-out;
+  }
+
+  h3 {
+    color: #9e9e9e; /* Substituído por uma cor específica */
+    font-size: 0.875rem;
+    margin-bottom: 0.5rem;
+    text-transform: uppercase;
+    text-align: center;
+    padding-bottom: 5vh;
+  }
+
+  .menu {
+    margin: 0 -1rem;
+
+    .button {
+      display: flex;
+      align-items: center;
+      text-decoration: none;
+      transition: 0.2s ease-in-out;
+      padding: 0.5rem 1rem;
+
+      .material-icons {
+        font-size: 2rem;
+        color: #272727; /* Substituído por uma cor específica */
+        transition: 0.2s ease-in-out;
+      }
+
+      .text {
+        color: #f0f0f0; /* Substituído por uma cor específica */
+        transition: 0.2s ease-in-out;
+      }
+
+      &:hover {
+        background-color: #212121; /* Substituído por uma cor específica */
+
+        .material-icons,
+        .text {
+          color: #ff0000; /* Substituído por uma cor específica */
+        }
+      }
+
+      &.router-link-exact-active {
+        background-color: transparent; /* Substituído por uma cor específica */
+        border-right: 5px solid #8f0000; /* Substituído por uma cor específica */
+
+        .material-icons,
+        .text {
+          color: #8f0000; /* Substituído por uma cor específica */
+        }
+      }
+    }
+  }
+
+  .footer {
+    opacity: 0;
+    transition: opacity 0.3s ease-in-out;
+
+    p {
+      font-size: 0.875rem;
+      color: #9e9e9e; /* Substituído por uma cor específica */
+    }
+  }
+
+  &.is-expanded {
+    width: var(--sidebar-width);
+
+    .menu-toggle-wrap {
+      top: -3rem;
+      
+      .menu-toggle {
+        transform: rotate(-180deg);
+      }
+    }
+
+    h3,
+    .button .text {
+      opacity: 1;
+    }
+
+    .button {
+      .material-icons {
+        margin-right: 1rem;
+      }
+    }
+
+    .footer {
+      opacity: 0;
+    }
+  }
+
+  @media (max-width: 1024px) {
+    position: absolute;
+    z-index: 99;
+  }
 }
 </style>
